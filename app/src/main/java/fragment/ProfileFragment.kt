b57eeed721b8737308b8aa6f4b7c38eb00f82fd0
rjpoh <93765344+rjpoh@users.noreply.github.com>
2022-04-12 19:06:32 +0800

    i feel like i wan die already who want to die with me

--- app/src/main/java/fragment/ProfileFragment.kt
+++ app/src/main/java/fragment/ProfileFragment.kt
@@ -21,12 +21,11 @@ class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    var colorCode:Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -36,6 +35,9 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+
     }
 
     companion object {
